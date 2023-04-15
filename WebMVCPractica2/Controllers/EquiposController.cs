6d80294544953fb9a0cecb6dcc2d23f601e858f8
Alejandro Toledo <88223531+WernerToledo@users.noreply.github.com>
2023-04-15 15:07:03 -0600

    Agregar archivos de proyecto.

---
+++ WebMVCPractica2/Controllers/EquiposController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCPractica01.Models;
+using WebMVCPractica2.Models;
+
+namespace WebMVCPractica2.Controllers
+{
+    public class EquiposController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Create([Bind()] equipos Dequipos)
+        {
+            return View("Index");
+        }
+    }
+}
---
+++ WebMVCPractica2/Models/EquiposContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVCPractica01.Models;
+using wedApi_join.Model;
+
+namespace WebMVCPractica2.Models
+{
+    public class equiposContext :DbContext
+    {
+        public equiposContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<tipo_equipo> tipo_equipo { get; set; }
+        public DbSet<equipos> equipos { get; set; }
+
+    }
+}
